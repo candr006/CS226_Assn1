877170409700c9d66c0cc50581d5f643788cb4b6
candr006 <candr006@ucr.edu>
2018-10-18 03:05:00 +0000

    arg checks

--- HDFSUpload/src/main/java/edu/ucr/cs/cs226/candr006/HDFSUpload.java
+++ HDFSUpload/src/main/java/edu/ucr/cs/cs226/candr006/HDFSUpload.java
@@ -1,13 +1,53 @@
 package edu.ucr.cs.cs226.candr006;
 
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FSDataOutputStream;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+
 /**
- * Hello world!
- *
+ * HDFSUpload class
+ * This class takes a path to a local input file and creates a copy in the given hdfs path
  */
 public class HDFSUpload 
 {
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	if(args.length<3){
+        	System.out.println("You are missing one or more arguments. Exiting.");
+        	return;
+    	}
+    	String str_local_file=args[1];
+    	String hdfs_path=args[2];
+
+    	File localFile= new File(str_local_file);
+		if(!localFile.exists()){
+			System.out.println("The local file you entered does not exist. Exiting.");
+		}
+
     }
 }
