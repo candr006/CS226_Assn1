b1e3330b1189e84e9628b748fa113bfe1ed7dc61
candr006 <candr006@ucr.edu>
2018-10-21 01:43:07 +0000

    attempt to read from hdfs file gives null pointer exception

--- HDFSUpload/src/main/java/edu/ucr/cs/cs226/candr006/HDFSUpload.java
+++ HDFSUpload/src/main/java/edu/ucr/cs/cs226/candr006/HDFSUpload.java
@@ -8,6 +8,7 @@ import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.fs.ftp.FTPFileSystem;
+import org.apache.hadoop.io.IOUtils;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
@@ -67,11 +68,11 @@ public class HDFSUpload
         }
 
 
+        //Copy file
+		long startTime = System.nanoTime();
 		java.nio.file.Path local_file_path = Paths.get(str_local_file);
 		InputStream is = new BufferedInputStream(new FileInputStream(localFile));
 		BZip2CompressorInputStream inputStream = new BZip2CompressorInputStream(is, true);
-
-
 		FSDataOutputStream ostream = fs.create(hdfsPath);
         final byte[] buffer = new byte[8192];
         int n = 0;
@@ -80,6 +81,30 @@ public class HDFSUpload
         }
         ostream.close();
         inputStream.close();
+		long estimatedTime = System.nanoTime() - startTime;
+		double seconds=estimatedTime/ 1000000000.0;
+		System.out.println("Seconds it takes to copy the local file to hdfs: "+seconds);
+
+
+
+		//Read the file we just copied to hdfs from start to finish
+		long startTime2 = System.nanoTime();
+		Configuration con2= new Configuration();
+		FileSystem fs2 = FileSystem.get(con2);
+		Path hdfsPath2 = new Path(str_hdfs_path);
+		InputStream in2 = null;
+		OutputStream out2=null;
+		try {
+			in2 = fs.open(hdfsPath2);
+			IOUtils.copyBytes(in2,out2, 4096, false);
+		} finally {
+			IOUtils.closeStream(in2);
+		}
+		in2.close();
+		long estimatedTime2 = System.nanoTime() - startTime2;
+		double seconds2=estimatedTime2/ 1000000000.0;
+		System.out.println("Seconds it takes to read the HDFS file: "+seconds);
+
 
 
         //Open the local file to read from and copy to hdfs location
